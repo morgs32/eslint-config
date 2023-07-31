4a63acf3acf9d614e073dbcc81270bb64a1a7bb8
Morgan <morgan.intrator@gmail.com>
2023-07-31 11:06:08 -0400

    fix: more from react-app

--- tests/Component.tsx
+++ tests/Component.tsx
@@ -2,12 +2,25 @@ interface IProps {
   
 }
 
+const projects = [
+  "foo",
+  "bar",
+]
+
 export function Component(props: IProps) {
 
   return (
     <div
       className="className"
     >
+      {projects.map((project) => (
+        <div
+          key={project}
+      >
+          {project}
+        </div>
+      ))       
+      }
     </div>
   )
   
