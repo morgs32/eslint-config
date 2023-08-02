284ea4496404149edd6bbd2ea398f01aea476dc2
Morgan <morgan.intrator@gmail.com>
2023-08-02 15:26:52 -0400

    fix: finally quitting the multiline props thing

--- tests/Component.tsx
+++ tests/Component.tsx
@@ -1,34 +1,26 @@
-interface IProps {
-  
-}
+import { useRef, useState, useEffect } from 'react'
+
+interface IProps {}
 
 const projects = [
   {
-    name: "foo"
+    name: 'foo',
   },
   {
-    name: "bar"
-  }
+    name: 'bar',
+  },
 ]
 
 export function Component(props: IProps) {
+  const [state, setState] = useState(0)
+  useEffect(() => {})
+  useRef()
 
   return (
-    <div
-      className="className"
-    >
-      {projects.map(({
-name: project
-  }) => (
-    <div
-      key={project}
-      >
-      {project}
-    </div>
-      ))       
-      }
+    <div className="className">
+      {projects.map(({ name: project }) => (
+        <div key={project}>{project}</div>
+      ))}
     </div>
   )
-  
-
 }
